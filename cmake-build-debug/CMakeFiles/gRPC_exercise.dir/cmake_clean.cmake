56e532869ecd8ac8b77d0dc93c14df72c1e79b71
bong <pgb8909@naver.com>
2021-09-01 11:20:08 +0900

    add cmake file

---
+++ cmake-build-debug/CMakeFiles/gRPC_exercise.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gRPC_exercise.dir/main.cpp.o"
+  "gRPC_exercise"
+  "gRPC_exercise.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gRPC_exercise.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/server.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/server.dir/server/Server.cpp.o"
+  "libserver.a"
+  "libserver.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
